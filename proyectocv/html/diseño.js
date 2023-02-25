91fcaf99797da80d1512d2d940e1fda984c11b34
ivannc <ivanninacondori96@gmail.com>
2023-02-25 17:50:40 -0300

    segundo intentode guardado

---
+++ proyectocv/html/diseño.js
@@ -0,0 +1,12 @@
+document.getElementById('boton').addEventListener('click', function() {
+    document.getElementById('demo').style.display = 'block'
+});
+document.getElementById('ocultar').addEventListener('click', function() {
+    document.getElementById('demo').style.display = 'none'
+});
+document.getElementById('botonb').addEventListener('click', function() {
+    document.getElementById('demob').style.display = 'block'
+});
+document.getElementById('ocultarb').addEventListener('click', function() {
+    document.getElementById('demob').style.display = 'none'
+});
